97379a7c5544bf7d75c40bc58394f219fb99bb85
Neon-Tetras <36481594+Neon-Tetras@users.noreply.github.com>
2018-03-07 15:22:26 +0100

    working on Enter remittance record activity

--- app/src/main/java/com/example/titomi/workertrackerloginmodule/inventory_module/inventory_sub_menu/ActivityAddRemit.java
+++ app/src/main/java/com/example/titomi/workertrackerloginmodule/inventory_module/inventory_sub_menu/ActivityAddRemit.java
@@ -2,26 +2,33 @@ package com.example.titomi.workertrackerloginmodule.inventory_module.inventory_s
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.titomi.workertrackerloginmodule.R;
 import com.example.titomi.workertrackerloginmodule.supervisor.User;
 import com.example.titomi.workertrackerloginmodule.supervisor.util.Util;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityAddRemit extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int MAX_IMAGE = 6;
     User loggedInUser;
     EditText amountText;
     TextView attachText;
     Context cxt;
+    LinearLayout proofMediaLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +36,7 @@ public class ActivityAddRemit extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_add_remit);
         amountText = findViewById(R.id.amountEdit);
         attachText = findViewById(R.id.attach);
+        proofMediaLayout = findViewById(R.id.proofMediaLayout);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         if(getIntent().getExtras() != null){
             loggedInUser = (User)getIntent().getExtras().getSerializable(getString(R.string.loggedInUser));
@@ -64,15 +72,84 @@ public class ActivityAddRemit extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if(requestCode == RESULT_OK && data !=null)
+        if(resultCode == RESULT_OK && data !=null)
         {
             switch (requestCode){
                 case Util.PICK_IMAGE_MULTIPLE:
+                    String[] imagesPath = data.getStringExtra("data").split("\\|");
+                    for (int i = 0; i < imagesPath.length; i++) {
+                        if (!canTakeImages()) {
+                            Toast.makeText(this,
+                                    String.format("Can only accept a maximum of %d images"
+                                            , MAX_IMAGE), Toast.LENGTH_LONG).show();
+                            break;
+                        }
+                        if (i >= MAX_IMAGE) {
+                            Toast.makeText(this,
+                                    String.format("Can only accept a maximum of %d images"
+                                            , MAX_IMAGE), Toast.LENGTH_LONG).show();
+                            break;
+                        }
 
+                        proofImages.add(imagesPath[i]);
+
+
+                    }
+
+
+                    loadFeaturedImages(proofImages);
 
                     break;
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+    private boolean canTakeImages(){
+        return proofImages.size() < MAX_IMAGE;
+    }
+
+    private void  loadFeaturedImages(final ArrayList<String> imageList){
+
+
+
+        proofMediaLayout.removeAllViews();
+
+
+        for(final String path : imageList) {
+
+
+            // featuredImages.add(path); //and new image to list
+            final View view = View.inflate(cxt,R.layout.report_image_single_item_with_delete_button,null);
+            final ImageView image =view.findViewById(R.id.reportImage);
+            ImageView delete = view.findViewById(R.id.delete);
+            image.setImageURI(Uri.parse(path));
+            image.setTag(path);
+
+
+            image.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            //image.setLeft(5);
+            image.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                    Util.viewImages(cxt,image,imageList);
+                }
+            });
+
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    proofMediaLayout.removeView(view);
+                    imageList.remove(path);
+                }
+            });
+
+
+            proofMediaLayout.addView(view);
+        }
+
+    }
+
+    ArrayList<String> proofImages = new ArrayList<>();
 }
