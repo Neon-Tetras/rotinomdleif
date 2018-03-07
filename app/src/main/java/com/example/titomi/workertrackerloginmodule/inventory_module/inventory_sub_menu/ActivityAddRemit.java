c2e84c490edf4468156e25fb487ba21d0c91e59f
Neon-Tetras <36481594+Neon-Tetras@users.noreply.github.com>
2018-03-07 15:00:30 +0100

    worked on remittance listing

--- app/src/main/java/com/example/titomi/workertrackerloginmodule/inventory_module/inventory_sub_menu/ActivityAddRemit.java
+++ app/src/main/java/com/example/titomi/workertrackerloginmodule/inventory_module/inventory_sub_menu/ActivityAddRemit.java
@@ -1,27 +1,40 @@
 package com.example.titomi.workertrackerloginmodule.inventory_module.inventory_sub_menu;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.titomi.workertrackerloginmodule.R;
 import com.example.titomi.workertrackerloginmodule.supervisor.User;
+import com.example.titomi.workertrackerloginmodule.supervisor.util.Util;
 
-public class ActivityAddRemit extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class ActivityAddRemit extends AppCompatActivity implements View.OnClickListener {
 
     User loggedInUser;
+    EditText amountText;
+    TextView attachText;
+    Context cxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        cxt = this;
         setContentView(R.layout.activity_add_remit);
-
+        amountText = findViewById(R.id.amountEdit);
+        attachText = findViewById(R.id.attach);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         if(getIntent().getExtras() != null){
             loggedInUser = (User)getIntent().getExtras().getSerializable(getString(R.string.loggedInUser));
         }
 
-
+        attachText.setOnClickListener(this);
     }
 
     @Override
@@ -41,5 +54,25 @@ public class ActivityAddRemit extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Util.pickMultiPhoto(cxt);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == RESULT_OK && data !=null)
+        {
+            switch (requestCode){
+                case Util.PICK_IMAGE_MULTIPLE:
+
+
+                    break;
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
